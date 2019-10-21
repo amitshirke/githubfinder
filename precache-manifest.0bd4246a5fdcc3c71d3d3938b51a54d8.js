cfabc15a3a08fb94e7692f0b63e9c3c5d257eac5
amitshirke <shirke.amit50@gmail.com>
2019-10-21 16:08:23 +0530

    Updates

---
+++ precache-manifest.0bd4246a5fdcc3c71d3d3938b51a54d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfd5d3c770ad2f321126a70e0ffeaf8",
+    "url": "/githubfinder/index.html"
+  },
+  {
+    "revision": "ca9db8cf1f985bf1a304",
+    "url": "/githubfinder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "5f46883d43a673e15f80",
+    "url": "/githubfinder/static/js/2.c5ec40bc.chunk.js"
+  },
+  {
+    "revision": "ca9db8cf1f985bf1a304",
+    "url": "/githubfinder/static/js/main.5f000514.chunk.js"
+  },
+  {
+    "revision": "69af9ac4ea908b096fc3",
+    "url": "/githubfinder/static/js/runtime~main.9a344206.js"
+  }
+]);
